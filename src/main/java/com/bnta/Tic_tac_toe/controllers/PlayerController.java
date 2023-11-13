c152082688708cfb9bb672b081ebe3f2791656a4
Emmanuel Ajayi <emmanuelajayi@Emmanuels-MacBook-Pro.local>
2023-11-13 17:11:37 +0000

    Wrote methods for playerService and playerController

--- src/main/java/com/bnta/Tic_tac_toe/controllers/PlayerController.java
+++ src/main/java/com/bnta/Tic_tac_toe/controllers/PlayerController.java
@@ -4,10 +4,12 @@ import com.bnta.Tic_tac_toe.models.Player;
 import com.bnta.Tic_tac_toe.models.PlayerDTO;
 import com.bnta.Tic_tac_toe.services.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/players")
@@ -18,27 +20,49 @@ public class PlayerController {
 
     @GetMapping
     public ResponseEntity<List<Player>> getAllPlayers(){
-        return null;
+        return new ResponseEntity<>(playerService.getAllPlayers(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Player> getPlayerById(@PathVariable long id){
-        return null;
+        Optional<Player> checkPlayer = playerService.getPlayerById(id);
+
+        if (checkPlayer.isPresent()){
+            return new ResponseEntity<>(checkPlayer.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+
     }
 
     @PostMapping
     public ResponseEntity<Player> addPlayer(@RequestBody Player player){
-        return null;
+        playerService.addPlayer(player);
+        return new ResponseEntity<>(player, HttpStatus.CREATED);
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Player> deletePlayerById(@PathVariable long id){
-        return null;
+    public ResponseEntity deletePlayerById(@PathVariable long id){
+        Optional<Player> checkPlayer = playerService.getPlayerById(id);
+
+        if (checkPlayer.isPresent()){
+            playerService.deleteById(id);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+
     }
 
-    @PatchMapping(value = "/{id}")
-    public ResponseEntity<Player> updatePlayerName(@PathVariable long id, @RequestBody PlayerDTO playerDTO){
-        return null;
+    @PatchMapping
+    public ResponseEntity<Player> updatePlayerName(@RequestBody Player player){
+        Optional<Player> checkPlayer = playerService.getPlayerById(player.getId());
+
+        if (checkPlayer.isPresent()){
+            return new ResponseEntity<>(playerService.updatePlayerName(player), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
 
--- src/main/java/com/bnta/Tic_tac_toe/services/PlayerService.java
+++ src/main/java/com/bnta/Tic_tac_toe/services/PlayerService.java
@@ -1,7 +1,42 @@
 package com.bnta.Tic_tac_toe.services;
 
+import com.bnta.Tic_tac_toe.models.Player;
+import com.bnta.Tic_tac_toe.models.PlayerDTO;
+import com.bnta.Tic_tac_toe.repositories.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerService {
+
+    @Autowired
+    PlayerRepository playerRepository;
+
+    public List<Player> getAllPlayers(){
+        return playerRepository.findAll();
+    }
+
+    public Optional<Player> getPlayerById(long id){
+        return playerRepository.findById(id);
+    }
+
+    public Player addPlayer(Player player){
+        playerRepository.save(player);
+        return player;
+    }
+
+    public void deleteById(long id){
+        playerRepository.deleteById(id);
+    }
+
+    public Player updatePlayerName(Player updatedPlayer){
+        Player player = playerRepository.findById(updatedPlayer.getId()).get();
+        player.setPlayerName(updatedPlayer.getPlayerName());
+        playerRepository.save(updatedPlayer);
+        return updatedPlayer;
+    }
+
 }
