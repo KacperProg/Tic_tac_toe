06e3e8a36b62ce30abcea6d74c17a21cf4ed1a91
Emmanuel Ajayi <emmanuelajayi@Emmanuels-MacBook-Pro.local>
2023-11-13 14:55:48 +0000

    Created Game class with constructors and getters and setters. Also created result enum.

--- src/main/java/com/bnta/Tic_tac_toe/models/Game.java
+++ src/main/java/com/bnta/Tic_tac_toe/models/Game.java
@@ -1,4 +1,80 @@
 package com.bnta.Tic_tac_toe.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "games")
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "player_id")
+    @JsonIgnoreProperties({"games"})
+    private Player player;
+
+    @Column
+    private boolean isComplete;
+
+    @Column
+    private Result result;
+
+    @OneToMany(mappedBy = "game")
+    @JsonIgnoreProperties({"game"})
+    private List<Cell> cells;
+
+    public Game(Player player) {
+        this.player = player;
+        this.isComplete = false;
+        this.cells = new ArrayList<>();
+    }
+
+    public Game() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public List<Cell> getCells() {
+        return cells;
+    }
+
+    public void setCells(List<Cell> cells) {
+        this.cells = cells;
+    }
 }
