10668254cb0766a58002dc121006ded77470b0ea
Kacper <kapelek82@outlook.com>
2023-12-03 16:24:10 +0000

    added UML diagram

--- Tic_tac_toe_code/src/main/java/com/bnta/Tic_tac_toe/models/Cell.java
+++ Tic_tac_toe_code/src/main/java/com/bnta/Tic_tac_toe/models/Cell.java
@@ -53,9 +53,7 @@ public class Cell {
         this.value = value;
     }
 
-    public int getCellNumber() {
-        return cellNumber;
-    }
+    public int getCellNumber() {return cellNumber;}
 
     public void setCellNumber(int cellNumber) {
         this.cellNumber = cellNumber;
